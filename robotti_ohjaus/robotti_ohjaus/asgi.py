6fc1c6d8c08f53649cb09b149912cdf84591d64e
Roo-M <156066355+Roo-M@users.noreply.github.com>
2026-06-01 14:46:03 +0300

    aloitus

---
+++ robotti_ohjaus/robotti_ohjaus/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for robotti_ohjaus project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'robotti_ohjaus.settings')
+
+application = get_asgi_application()
